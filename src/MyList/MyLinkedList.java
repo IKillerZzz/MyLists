d985f94ec7c90f4aeb851c2ca1e7906991c737e7
Illia Paliachynski <aliveshooter@gmail.com>
2025-04-14 13:46:01 +0300

    docs: add javadoc

--- src/MyList/MyLinkedList.java
+++ src/MyList/MyLinkedList.java
@@ -3,16 +3,31 @@ package MyList;
 import java.util.Arrays;
 import java.util.Iterator;
 
+/**
+ * Этот класс предоставляет реализацию интерфейса List для минимизации усилий,
+ * необходимых для реализации этого интерфейса, поддерживаемого хранилищем данных
+ * "последовательного доступа".
+ * @author Illia Paliachynski
+ * @param <E> - тип элементов в списке.
+ */
 public class MyLinkedList<E> implements MyList<E> {
     private Node<E> head;
     private Node<E> tail;
     private int size = 0;
 
+    /**
+     * Конструктор по умолчанию, который задает голову и хвост списка.
+     */
     public MyLinkedList() {
         head = new Node<E>(null, null, tail);
         tail = new Node<E>(head, null, null);
     }
 
+    /**
+     * Метод, который добавляет указанный в параметре элемент в конец списка.
+     * @param element - элемент, который будет добавлен в конец списка.
+     * @return {@code true} если элемент был успешно добавлен.
+     */
     @Override
     public boolean add(E element) {
         if (size == 0) {
@@ -29,6 +44,11 @@ public class MyLinkedList<E> implements MyList<E> {
         return true;
     }
 
+    /**
+     * Добавляет указанный в параметрах элемент на указанную в параметрах позицию в списке.
+     * @param index - позиция в списке, на которую необходимо добавить элемент.
+     * @param element - элемент, который необходимо добавить в список.
+     */
     @Override
     public void add(int index, E element) {
         isInBounds(index);
@@ -40,6 +60,11 @@ public class MyLinkedList<E> implements MyList<E> {
         size++;
     }
 
+    /**
+     * Возвращает элемента из списка, по указанной в параметрах позиции.
+     * @param index - позиция, по которой нужно получить элемент из списка.
+     * @return - элемент из списка, который находится по указанной в параметрах позиции.
+     */
     @Override
     public E get(int index) {
         isInBounds(index);
@@ -50,6 +75,12 @@ public class MyLinkedList<E> implements MyList<E> {
         return currentNode.getCurrentElement();
     }
 
+    /**
+     * Удаляет первое вхождение указанного в параметрах элемента в списке.
+     * @param object - элемент, первое вхождение которого необходимо удалить из списка.
+     * @return {@code true} - если указанный элемент был успешно удален. {@code false} - если указанного
+     * элемента нет в списке.
+     */
     @Override
     public boolean remove(Object object) {
         E element = (E) object;
@@ -62,6 +93,11 @@ public class MyLinkedList<E> implements MyList<E> {
         return false;
     }
 
+    /**
+     * Удаляет элемент из списка, по указанному в параметре индексу.
+     * @param index - позиция элемента, который необходимо удалить из списка.
+     * @return - возвращает удаленный из списка элемент.
+     */
     @Override
     public E remove(int index) {
         if (isInBounds(index)) {
@@ -77,11 +113,21 @@ public class MyLinkedList<E> implements MyList<E> {
         return null;
     }
 
+    /**
+     * Возвращает текущий размер списка.
+     * @return целочисленное значение, которое соответствует актуальному размеру списка.
+     */
     @Override
     public int size() {
         return size;
     }
 
+    /**
+     * Задает существующему элементу указанное в параметрах значение element по заданному в параметрах индексу index.
+     * @param index - позиция элемента в списке, который требуется изменить.
+     * @param element - новое значение, которое будет задано элементу по указанному индексу.
+     * @return элемент, который был успешно изменен.
+     */
     @Override
     public E set(int index, E element) {
         Node<E> targetNode = getNode(index);
@@ -89,6 +135,12 @@ public class MyLinkedList<E> implements MyList<E> {
         return targetNode.getCurrentElement();
     }
 
+    /**
+     * Возвращает новый список, который находится в заданном в параметрах диапазоне текущего списка.
+     * @param fromIndex - начальная позиция существующего списка, с которой будут получены элементы в новый список.
+     * @param toIndex - позиция конечного элемента, по который будут получены элементы в новый список.
+     * @return новый список, который находится в границах от fromIndex по toIndex текущего списка.
+     */
     @Override
     public MyLinkedList<E> subList(int fromIndex, int toIndex) {
         MyLinkedList<E> subList = new MyLinkedList<E>();
@@ -103,6 +155,12 @@ public class MyLinkedList<E> implements MyList<E> {
         return null;
     }
 
+    /**
+     * Проверяет, находиться ли указанный в параметрах index в допустимом диапазоне.
+     * @param index - значение index, которое проверяется на допустимость.
+     * @throws IndexOutOfBoundsException - если значение index выходит за рамки допустимого диапазона.
+     * @return {@code true}, если значение index находится в рамках допустимого диапазона.
+     */
     private boolean isInBounds(int index) {
         if (index < 0 || index > size - 1) {
             throw new IndexOutOfBoundsException("Некорректное значение index: " + index + ". Индекс не может быть < 0 и больше чем " + (size - 1));
@@ -110,6 +168,11 @@ public class MyLinkedList<E> implements MyList<E> {
         return true;
     }
 
+    /**
+     * Возвращает объект Node, который назодится по указанному в параметрах индексу.
+     * @param index - целочисленное значение, по позиции которому нужно поучить узел из списка.
+     * @return объект Node, который находится по заданному индексу.
+     */
     private Node<E> getNode(int index) {
         isInBounds(index);
         Node<E> currentNode = head.getNextElement();
@@ -119,6 +182,10 @@ public class MyLinkedList<E> implements MyList<E> {
         return currentNode;
     }
 
+    /**
+     * Возвращает строковое представление списка элементов.
+     * @return строковое представление списка элементов.
+     */
     @Override
     public String toString() {
         E[] array = (E[]) new Object[size];
@@ -128,42 +195,81 @@ public class MyLinkedList<E> implements MyList<E> {
         return Arrays.toString(array);
     }
 
+    /**
+     * Возвращает итератор, для обхода текущего списка, передавая в него ссылку на текущий объект списка.
+     * @return объект итератора для последовательного обхода текущего списка.
+     */
     @Override
     public Iterator<E> iterator() {
         return new MyLinkedListIterator<E>(this);
     }
 
+    /**
+     * Внутренний статический класс представление узлов (элементов) списка.
+     * Хранит ссылку на узел предыдущего и следующего элемента, а также значение текущего элемента.
+     * @param <E> - тип элементов в списке
+     */
     private static class Node<E> {
         Node<E> prevElement;
         E currentElement;
         Node<E> nextElement;
 
+        /**
+         * Конструктор, который принимает следующие параметры:
+         * @param prevElement - ссылка на узел предыдущего элемента списка.
+         * @param currentElement - значение элемента текущего узла списка.
+         * @param nextElement - ссылка на узел следующего в списке элемента.
+         */
         public Node(Node<E> prevElement, E currentElement, Node<E> nextElement) {
             this.prevElement = prevElement;
             this.currentElement = currentElement;
             this.nextElement = nextElement;
         }
 
+        /**
+         * Устанавливает текущему элементу ссылку на узел предыдущего элемента списка
+         * @param node - ссылка на узел предыдущего элемента
+         */
         public void setPrevElement(Node<E> node) {
             prevElement = node;
         }
 
+        /**
+         * Устанавливает текущему узлу списка значение переданного в параметрах элемента.
+         * @param element - значение для текущего элемента списка.
+         */
         public void setCurrentElement(E element) {
             currentElement = element;
         }
 
+        /**
+         * Устанавливает текущему элементу ссылку на узел следующего элемента списка
+         * @param node - ссылка на узел следующего элемента списка.
+         */
         public void setNextElement(Node<E> node) {
             nextElement = node;
         }
 
+        /**
+         * Возвращает ссылку на узел предыдущего элемента списка.
+         * @return ссылку на узел предыдущего элемента
+         */
         public Node<E> getPrevElement() {
             return prevElement;
         }
 
+        /**
+         * Возвращает значение элемента для текущего узла списка.
+         * @return значение элемента списка для текущего узла.
+         */
         public E getCurrentElement() {
             return currentElement;
         }
 
+        /**
+         * Возвращает ссылку на узел следующего элемента списка.
+         * @return ссылку на узел следующего элемента
+         */
         public Node<E> getNextElement() {
             return nextElement;
         }
--- src/MyList/MyLinkedListIterator.java
+++ src/MyList/MyLinkedListIterator.java
@@ -2,19 +2,36 @@ package MyList;
 
 import java.util.Iterator;
 
+/**
+ * Итератор для класса MyLinkedList, для последовательного обхода элементов списка.
+ * @author Illia Paliachynski
+ * @param <E> - тип элементов в списке.
+ */
 public class MyLinkedListIterator<E> implements Iterator<E> {
     MyLinkedList<E> list;
     int counter = 0;
 
+    /**
+     * Конструктор итератора, который принимает следующий параметр:
+     * @param list - объект списка элементов, по которому будет осуществляться итерация.
+     */
     public MyLinkedListIterator(MyLinkedList<E> list) {
         this.list = list;
     }
 
+    /**
+     * Метод, который проверяет наличие следующего в списке элемента.
+     * @return {@code true} - если следующий элемент существует. {@code false} - если в списке больше нет элементов.
+     */
     @Override
     public boolean hasNext() {
         return counter < list.size();
     }
 
+    /**
+     * Возвращает следующий элемент списка.
+     * @return следующий элемент в списке.
+     */
     @Override
     public E next() {
         return list.get(counter++);
